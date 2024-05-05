b8e89a549dd660d4bc8c8af901ee76a8965eeea6
Samir0704 <167703733+Samir0704@users.noreply.github.com>
2024-05-06 02:10:22 +0500

    Update hmw2.py

--- hmw2.py
+++ hmw2.py
@@ -1,55 +1,55 @@
-# import threading
-#
-#
-# def print_cube(num):
-#     print("Cube: {}".format(num * num * num))
-#
-#
-# def print_square(num):
-#     print("Square: {}".format(num * num))
-#
-#
-# if __name__ == "__main__":
-#     t1 = threading.Thread(target=print_square, args=(10,))
-#     t2 = threading.Thread(target=print_cube, args=(10,))
-#
-#     t1.start()
-#     t2.start()
-#
-#     t1.join()
-#     t2.join()
-#
-#     print("Done!")
+import threading
 
-#
-# import threading
-# import os
-#
-#
-# def task1():
-#     print("1-topshiriq ipga tayinlangan: {}".format(threading.current_thread().name))
-#     print("Jarayonni bajaruvchi vazifa identifikatori 1: {}".format(os.getpid()))
-#
-#
-# def task2():
-#     print("2-topshiriq ipga tayinlangan: {}".format(threading.current_thread().name))
-#     print("Jarayonni bajaruvchi vazifa identifikatori 2: {}".format(os.getpid()))
-#
-#
-# if __name__ == "__main__":
-#         print("Asosiy dasturni ishga tushiradigan jarayon identifikatori: {}".format(os.getpid()))
-#
-#     print("Asosiy mavzu nomi: {}".format(threading.current_thread().name))
-#
-#     t1 = threading.Thread(target=task1, name='t1')
-#     t2 = threading.Thread(target=task2, name='t2')
-#
-#     t1.start()
-#     t2.start()
-#
-#     t1.join()
-#     t2.join()
-#
+
+def print_cube(num):
+    print("Cube: {}".format(num * num * num))
+
+
+def print_square(num):
+    print("Square: {}".format(num * num))
+
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target=print_square, args=(10,))
+    t2 = threading.Thread(target=print_cube, args=(10,))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print("Done!")
+
+
+import threading
+import os
+
+
+def task1():
+    print("1-topshiriq ipga tayinlangan: {}".format(threading.current_thread().name))
+    print("Jarayonni bajaruvchi vazifa identifikatori 1: {}".format(os.getpid()))
+
+
+def task2():
+    print("2-topshiriq ipga tayinlangan: {}".format(threading.current_thread().name))
+    print("Jarayonni bajaruvchi vazifa identifikatori 2: {}".format(os.getpid()))
+
+
+if __name__ == "__main__":
+        print("Asosiy dasturni ishga tushiradigan jarayon identifikatori: {}".format(os.getpid()))
+
+    print("Asosiy mavzu nomi: {}".format(threading.current_thread().name))
+
+    t1 = threading.Thread(target=task1, name='t1')
+    t2 = threading.Thread(target=task2, name='t2')
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
 
 import time
 import threading
@@ -74,6 +74,10 @@ t2.start()
 
 t1.join()
 t2.join()
+
+
+
+
 
 
 
